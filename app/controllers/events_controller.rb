9c959ac755b6162a5dc1d29ea961e3119cc4ee54
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-09 18:31:21 -0800

    update action added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,4 +10,13 @@ class EventsController < ApplicationController
   def edit
   	@event = Event.find(params[:id])
   end
+
+  def update
+  	@event = Event.find(params[:id])
+  	# only allow certain attributes
+  	event_params = params.require(:event).
+  									permit(:name, :description, :location, :price, :starts_at)
+  	@event.update(event_params)
+  	redirect_to event_path(@event)	#this resolves to show action
+  end
 end
